0c5dfe873621d12f5e1997e4015e715f6996d82e
Vlad4777 <133170064+Vlad4777@users.noreply.github.com>
2025-12-10 18:45:18 +0100

    Update hook.js

--- src/hook.js
+++ src/hook.js
@@ -19,6 +19,35 @@
       window.$media.addEventListener(type, listener);
     }
     browser.runtime.onMessage.addListener((message) => {
+      // from popup
+if (msg.type === "serial-connect") {
+  openSerialByIndex(msg.idx).then((ok) => {
+    browser.runtime.sendMessage({
+      type: "serial-state",
+      connected: ok,
+      info: ok ? serialPort.getInfo() : null
+    });
+  });
+  return;
+}
+
+if (msg.type === "serial-disconnect") {
+  closeSerial().then(() => {
+    browser.runtime.sendMessage({
+      type: "serial-state",
+      connected: false
+    });
+  });
+  return;
+}
+
+if (msg.type === "serial-query") {
+  return Promise.resolve({
+    connected: !!serialPort,
+    info: serialPort ? serialPort.getInfo() : null
+  });
+}
+
       if (message === "@unhook") {
         const $media = document.querySelector("[mcx-media]");
         if ($media !== null) {
